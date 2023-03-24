1b7f936552438714a8d6abc3bcb8bc3de15a13b1
unigwesunday7 <unigwesunday7@gmail.com>
2023-03-24 23:24:00 +0100

    Initial commit

---
+++ calc.js
@@ -0,0 +1,46 @@
+let numField1 = document.getElementById('input1');
+let numField2 = document.getElementById('input2');
+let form = document.getElementById('calcForm');
+
+form.addEventListener('submit', function(event) {
+
+    if (document.getElementById("calcSelect").selectedIndex == "0"){
+        var x = parseFloat(input1.value);
+        var y = parseFloat(input2.value);
+        var result = y / 100 * x;
+        calcResult.innerText = y + " % of " + x + " is " + result;
+        event.preventDefault();
+    } 
+    
+    else if (document.getElementById("calcSelect").selectedIndex == "1"){
+        var x = parseFloat(input1.value);
+        var y = parseFloat(input2.value);
+        var result = x * 100 / y;
+        calcResult.innerText = x + " is " + y + " % of " + result;
+        event.preventDefault();
+    } 
+    
+    else if (document.getElementById("calcSelect").selectedIndex == "2"){
+        var x = parseFloat(input1.value);
+        var y = parseFloat(input2.value);
+        var result = y / x * 100;
+        calcResult.innerText = result + "% of " + x + " is " + y;
+        event.preventDefault();
+    } 
+    
+    else if (document.getElementById("calcSelect").selectedIndex == "3"){
+        var x = parseFloat(input1.value);
+        var y = parseFloat(input2.value);
+        var result = y / x * 100;
+        calcResult.innerText = x + "% of " + result + " is " + y;
+        event.preventDefault();
+    } 
+    
+    else if (document.getElementById("calcSelect").selectedIndex == "4"){
+        var x = parseFloat(input1.value);
+        var y = parseFloat(input2.value);
+        var result = y / 100 * x;
+        calcResult.innerText = y + "% of " + x + " is " + result;
+        event.preventDefault();
+    }
+});
