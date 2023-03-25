47f1a652ae3e8244f49090ddcc317464f995d0fa
unigwesunday7 <unigwesunday7@gmail.com>
2023-03-25 09:30:18 +0100

    Form validation

--- calc.js
+++ calc.js
@@ -4,11 +4,17 @@ let form = document.getElementById('calcForm');
 
 form.addEventListener('submit', function(event) {
 
-    if (document.getElementById("calcSelect").selectedIndex == "0"){
+    
+    if (!numField1.value || !numField2.value) {
+        calcResult.innerText = "Please enter values in the fields"
+        event.preventDefault();
+    }
+    else if (document.getElementById("calcSelect").selectedIndex == "0"){
         var x = parseFloat(input1.value);
         var y = parseFloat(input2.value);
         var result = y / 100 * x;
-        calcResult.innerText = y + " % of " + x + " is " + result;
+        calcResult.innerText = "Result: " + y + "% of " + x + " = " + result;
+        sumary.innerText = `Equation:  y / 100 * x = ${result}`;
         event.preventDefault();
     } 
     
@@ -17,6 +23,7 @@ form.addEventListener('submit', function(event) {
         var y = parseFloat(input2.value);
         var result = x * 100 / y;
         calcResult.innerText = x + " is " + y + " % of " + result;
+        sumary.innerText = `Equation:  x * 100 / x`;
         event.preventDefault();
     } 
     
@@ -25,6 +32,7 @@ form.addEventListener('submit', function(event) {
         var y = parseFloat(input2.value);
         var result = y / x * 100;
         calcResult.innerText = result + "% of " + x + " is " + y;
+        sumary.innerText = `Equation:  y / x * 100`;
         event.preventDefault();
     } 
     
@@ -33,6 +41,7 @@ form.addEventListener('submit', function(event) {
         var y = parseFloat(input2.value);
         var result = y / x * 100;
         calcResult.innerText = x + "% of " + result + " is " + y;
+        sumary.innerText = `Equation:  y / x * 100`;
         event.preventDefault();
     } 
     
@@ -41,6 +50,7 @@ form.addEventListener('submit', function(event) {
         var y = parseFloat(input2.value);
         var result = y / 100 * x;
         calcResult.innerText = y + "% of " + x + " is " + result;
+        sumary.innerText = `Equation:  y / 100 * x`;
         event.preventDefault();
     }
 });
